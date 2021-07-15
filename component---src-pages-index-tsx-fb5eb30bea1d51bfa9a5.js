2651721e5b7efba193d261fe34f212dfd7cf277a
Melvin <melvin.sy@outlook.com>
2021-07-15 21:56:44 +0800

    Updates

---
+++ component---src-pages-index-tsx-fb5eb30bea1d51bfa9a5.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmebyn_github_io=self.webpackChunkmebyn_github_io||[]).push([[691],{738:function(e,n,l){"use strict";l.r(n),l.d(n,{default:function(){return u}});var t=l(294);function u(){return t.createElement(t.Fragment,null,t.createElement("h1",null,"Melvin's Personal Website!"),t.createElement(r,null))}function r(){return t.createElement("ul",null,t.createElement("li",null,"melvin.sy@outlook.com"),t.createElement("li",null,"I make computers beep boop"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-fb5eb30bea1d51bfa9a5.js.map
